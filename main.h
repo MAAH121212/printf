deea58aa0781c1d25fbb7ad2856170db54788224
Your Name <you@example.com>
2023-08-23 09:02:47 -0700

    pop

--- main.h
+++ main.h
@@ -96,7 +96,7 @@ int print_number_left_shift(char *str, params_t *params);
 
 void init_params(params_t *params, va_list ap);
 
-char get_precision(char *p, params_t *params, va_list ap);
+char *get_precision(char *p, params_t *params, va_list ap);
 
 int _printf(const char *format, ...);
 
