5bf28aed5fa5d7d7bf8145351abf93ce7e6631d0
hamido <clyde_python@yahoo.com>
2023-08-24 15:29:57 -0700

    ya rab

--- specifier.c
+++ specifier.c
@@ -35,3 +35,75 @@ int (*get_specifier(char *s)) (va_list ap, params_t *params)
 		}
 		return (NULL);
 }
+
+int get_print_func(char *s, va_list ap, params_t *params)
+{
+	int (*f)(va_list, params_t *) = get_specifier(s);
+
+	if (f)
+		return (f(ap, params));
+	return (0);
+}
+
+int get_flag(char *s, params_t *params)
+{
+	int i = 0
+		switch (*s)
+{
+	case '+':
+	i = params->plus_flag = 1;
+	break;
+
+	case '-':
+        i = params->minus_flag = 1;
+        break;
+
+	case ' ':
+        i = params->space_flag = 1;
+        break;
+
+	case '#':
+        i = params->hashtag_flag = 1;
+        break;
+
+	case '0':
+        i = params->zero_flag = 1;
+        break;
+}
+return (i);
+}
+
+int get_modifier (char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+{
+		case 'h':
+        i = params->h_modifier = 1;
+        break;
+
+	case 'l':
+        i = params->l_modifier = 1;
+        break;
+}
+return (i);
+}
+
+char *get_width(char *s, params_t *params, va_list ap)
+{
+	int d = 0;
+
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->width = d;
+	return (s);
+}
